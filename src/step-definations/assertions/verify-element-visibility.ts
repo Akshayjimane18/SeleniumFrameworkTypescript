97a7624b93dc1d6299203bd78b377bc31f0e852a
akshayjimane18 <akshayjimane18@gmail.com>
2023-05-14 19:38:20 +0530

    14th May code updated

--- src/step-definations/assertions/verify-element-visibility.ts
+++ src/step-definations/assertions/verify-element-visibility.ts
@@ -5,14 +5,11 @@ import {By} from 'selenium-webdriver';
 Then(/^the contacts header should contain the text contacts$/,
     async function () {
 
-        console.log("The contacts header should contain the text contacts");
+            console.log("The contacts header should contain the text contacts");
 
         const element = await global.myDriver.findElement(By.css("[data-id='contacts']"));
 
         const elementText = await element.getAttribute('innerText');
 
         expect(elementText).to.contain("Contacts");
-    
-
-    }
-)
+        });
